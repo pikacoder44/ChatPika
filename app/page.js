c1403eca9375bbd02c653d3d9bc5f4286f834d1b
Syed Hashir Ali <pikaop125@gmail.com>
2025-08-22 23:11:59 +0500

    Configured OpenAI API in Routes

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
-
+"use client";
+import { useState } from "react";
 export default function Home() {
+  const [message, setMessage] = useState("");
   return (
     <div className="">
       <h1>Hello World</h1>
