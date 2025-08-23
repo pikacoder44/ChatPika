fe8c780b3efc05c71019f29e5519572a65684fd1
Syed Hashir Ali <pikaop125@gmail.com>
2025-08-23 19:27:12 +0500

    Updated ChatWindow UI

--- app/components/ChatWindow.tsx
+++ app/components/ChatWindow.tsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 
 const ChatWindow = (props) => {
+    let [userMessage,setuserMessage] = useState(props.user);
+
   return (
     <div className="w-[60%] mx-auto h-200 flex flex-col gap-3 p-3 ">
       <div className="flex flex-col gap-3 p-3 ">
         {props.sent && (
           <div className="flex justify-end">
             <div className="bg-blue-500 text-white px-4 py-2 rounded-xl max-w-[70%] break-words">
-              {props.user}
+              {userMessage}
             </div>
           </div>
         )}
