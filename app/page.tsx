13be37a584aa2a857a715bd5fbc259f5f4d66911
Syed Hashir Ali <pikaop125@gmail.com>
2025-08-23 21:32:42 +0500

    Made UI of Landing Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,33 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 export default function Home() {
   return (
-    <div className="p-6 w-full min-h-[989] flex flex-col items-center  dark:bg-zinc-900 dark:text-white">
-      <h1 className="text-4xl">
-        Welcome to Home Page of ChatPika
-        </h1>
+    <div className="p-6 w-full min-h-[989] flex flex-col items-center   dark:bg-zinc-900 dark:text-white">
+      <h1 className="text-6xl mt-6 font-medium">Welcome to Home Page of ChatPika</h1>
+      <Link href={"/chat"}>
+        <Button variant="outline" size="sm" className=" mt-6 hover:cursor-pointer">
+          Try ChatPik{" "}
+          <svg
+            className="w-6 h-6 text-gray-800 dark:text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M19 12H5m14 0-4 4m4-4-4-4"
+            />
+          </svg>
+        </Button>
+      </Link>
     </div>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,17 @@
 import React from "react";
-
+import Link from "next/link";
 const Navbar = () => {
   return (
-    <div className= " p-5 dark:bg-zinc-900 dark:text-white">
+    <div className=" p-5 dark:bg-zinc-900 dark:text-white">
       <nav className="flex justify-between  flex-row ">
-        <h1 className=" font-bold text-4xl">ChatPika</h1>
+        <Link href="/" className="hover:cursor-pointer">
+          <h1 className=" font-bold text-4xl">ChatPika</h1>
+        </Link>
         <ul className="flex flex-row justify-between">
-          <li className="p-2 m-2">Home</li>
+          <Link href="/">
+            {" "}
+            <li className="p-2 m-2">Home</li>
+          </Link>
           <li className="p-2 m-2">About</li>
           <li className="p-2 m-2">Contact</li>
         </ul>
