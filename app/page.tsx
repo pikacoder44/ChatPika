d19a3cafc7324ca9bca9f12308c28fd2560c7ba1
Syed Hashir Ali <pikaop125@gmail.com>
2025-08-23 20:56:51 +0500

    Added Shadcn

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Home() {
+  return (
+    <div className="p-6 w-full min-h-[989] flex flex-col items-center  dark:bg-zinc-900 dark:text-white">
+      <h1 className="text-4xl">
+        Welcome to Home Page of ChatPika
+        </h1>
+    </div>
+  );
+}
