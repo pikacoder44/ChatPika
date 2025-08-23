e6308562a3cdcef2613deef56bb0b5293f6f5db6
Syed Hashir Ali <pikaop125@gmail.com>
2025-08-23 22:45:37 +0500

    Tried to add CSS

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,39 @@
 "use client";
-
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 export default function Home() {
+  const faq = [
+    {
+      question: "Why use ChatPika?",
+      answer: "Coz you are not GAY.",
+    },
+    {
+      question: "Is it free?",
+      answer: "Yes, It's free trial is of 400 tokens.",
+    },
+    {
+      question: "How can we chat?",
+      answer: "Just go to /chat to chat and login to keep records",
+    },
+  ];
+
   return (
     <div className="p-6 w-full min-h-[989] flex flex-col items-center   dark:bg-zinc-900 dark:text-white">
-      <h1 className="text-6xl mt-6 font-medium">Welcome to Home Page of ChatPika</h1>
+      <h1 className="text-6xl mt-6 font-medium">
+        Welcome to Home Page of ChatPika
+      </h1>
       <Link href={"/chat"}>
-        <Button variant="outline" size="sm" className=" mt-6 hover:cursor-pointer">
+        <Button
+          variant="outline"
+          size="sm"
+          className=" mt-6 hover:cursor-pointer"
+        >
           Try ChatPik{" "}
           <svg
             className="w-6 h-6 text-gray-800 dark:text-white"
@@ -20,14 +46,24 @@ export default function Home() {
           >
             <path
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M19 12H5m14 0-4 4m4-4-4-4"
             />
           </svg>
         </Button>
       </Link>
+
+      <h2 className="text-3xl mt-6 font-medium">Frequently Asked Questions</h2>
+      <Accordion type="single" collapsible>
+        {faq.map((item, index) => (
+          <AccordionItem key={index} value={`item-${index}`}>
+            <AccordionTrigger>{item.question}</AccordionTrigger>
+            <AccordionContent>{item.answer}</AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
     </div>
   );
 }
