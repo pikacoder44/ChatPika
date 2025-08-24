34b0dd2153c23780b6cb481b2889016974fb5ed5
Syed Hashir Ali <pikaop125@gmail.com>
2025-08-24 17:00:18 +0500

    Added Scrollbar to Chat

--- components/ChatWindow.tsx
+++ components/ChatWindow.tsx
@@ -1,28 +1,43 @@
-// components/ChatWindow.tsx
 import ReactMarkdown from "react-markdown";
-
+import * as ScrollArea from "@radix-ui/react-scroll-area";
 const ChatWindow = ({ messages }) => {
   return (
-    <div className="w-[60%] mx-auto h-200 flex flex-col gap-3 p-3 border rounded-md overflow-y-auto">
-      {messages.map((msg, index) => (
-        <div
-          key={index}
-          className={`flex ${
-            msg.role === "user" ? "justify-end" : "justify-start"
-          }`}
-        >
-          <div
-            className={`px-4 py-2 rounded-xl max-w-[70%] break-words ${
-              msg.role === "user"
-                ? "bg-blue-500 text-white"
-                : "bg-zinc-800 text-white"
-            }`}
-          >
-            {msg.role === "user" ? "User" : "AI"}:{" "}
-            <ReactMarkdown>{msg.content}</ReactMarkdown>
+    <div className="flex-1 w-full flex justify-center">
+      <ScrollArea.Root className=" ScrollAreaRoot w-[70%] h-[850px] rounded-md overflow-hidden ">
+        <ScrollArea.Viewport className="ScrollAreaViewport w-full h-full p-3">
+          <div className="w-[80%] mx-auto h-[850px] flex flex-col gap-3 p-3  rounded-md ">
+            {messages.map((msg, index) => (
+              <div
+                key={index}
+                className={`flex ${
+                  msg.role === "user" ? "justify-end" : "justify-start"
+                }`}
+              >
+                <div
+                  className={`px-4 py-2 rounded-xl max-w-[70%] break-words ${
+                    msg.role === "user"
+                      ? "bg-blue-500 text-white"
+                      : "bg-zinc-800 text-white"
+                  }`}
+                >
+                  {msg.role === "user" ? "User" : "AI"}:{" "}
+                  <ReactMarkdown>{msg.content}</ReactMarkdown>
+                </div>
+              </div>
+            ))}
           </div>
-        </div>
-      ))}
+        </ScrollArea.Viewport>
+
+        {/* Vertical Scrollbar */}
+        <ScrollArea.Scrollbar
+          orientation="vertical"
+          className="ScrollAreaScrollbar flex select-none touch-none w-20  p-0.5 transition-colors duration-150 "
+        >
+          <ScrollArea.Thumb className="ScrollAreaThumb bg-gray-400 rounded-full hover:bg-blue-300"  />
+        </ScrollArea.Scrollbar>
+
+        <ScrollArea.Corner className="ScrollAreaCorner bg-blue-500" />
+      </ScrollArea.Root>
     </div>
   );
 };
