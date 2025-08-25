56a7a8b569a94c80b0913d04d0383f7abc8dd77a
Syed Hashir Ali <pikaop125@gmail.com>
2025-08-25 07:17:31 +0500

    Added heading/subheading to HomePage & Fized WelcomeChat stroke problems

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,10 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import CTA from "@/components/CallToAction";
 export default function Home() {
   const faq = [
     {
@@ -25,35 +27,12 @@ export default function Home() {
 
   return (
     <div className="p-6 w-full min-h-[989] flex flex-col items-center   dark:bg-zinc-900 dark:text-white">
-      <h1 className="text-6xl mt-6 font-medium ">
-        Welcome to Home Page of ChatPika
+      <h1 className="text-6xl mt-6 text-amber-300 font-medium ">
+        Chat with your personal AI assistant
       </h1>
-      <Link href={"/chat"}>
-        <Button
-          variant="outline"
-          size="sm"
-          className=" mt-6 hover:cursor-pointer"
-        >
-          Try ChatPik{" "}
-          <svg
-            className="w-6 h-6 text-gray-800 dark:text-white"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M19 12H5m14 0-4 4m4-4-4-4"
-            />
-          </svg>
-        </Button>
-      </Link>
+      <p className="mt-2 text-3xl">“Built with Next.js + AI”</p>
+
+      <CTA/>
 
       <h2 className="text-3xl mt-6 font-medium">Frequently Asked Questions</h2>
       <Accordion type="single" collapsible>
