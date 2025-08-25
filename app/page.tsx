d5f231b53c9d019261ce6683ab82a846a18cd230
Syed Hashir Ali <pikaop125@gmail.com>
2025-08-25 13:50:39 +0500

    Deleted Shadcn Accordion

--- app/page.tsx
+++ app/page.tsx
@@ -10,20 +10,7 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import CTA from "@/components/CallToAction";
 export default function Home() {
-  const faq = [
-    {
-      question: "Why use ChatPika?",
-      answer: "Coz you are not GAY.",
-    },
-    {
-      question: "Is it free?",
-      answer: "Yes, It's free trial is of 400 tokens.",
-    },
-    {
-      question: "How can we chat?",
-      answer: "Just go to /chat to chat and login to keep records",
-    },
-  ];
+  
 
   return (
     <div className="p-6 w-full min-h-[989] flex flex-col items-center   dark:bg-zinc-900 dark:text-white">
@@ -35,14 +22,7 @@ export default function Home() {
       <CTA/>
 
       <h2 className="text-3xl mt-6 font-medium">Frequently Asked Questions</h2>
-      <Accordion type="single" collapsible>
-        {faq.map((item, index) => (
-          <AccordionItem key={index} value={`item-${index}`}>
-            <AccordionTrigger>{item.question}</AccordionTrigger>
-            <AccordionContent>{item.answer}</AccordionContent>
-          </AccordionItem>
-        ))}
-      </Accordion>
+      
     </div>
   );
 }
