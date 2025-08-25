4e28764e8064d17fe6e6779a50eb2be7ff73e73c
Syed Hashir Ali <pikaop125@gmail.com>
2025-08-25 07:16:05 +0500

    Made Call to Action Comp for Home Page

---
+++ components/CallToAction.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+const CTA = () => {
+  return (
+    <div className="relative overflow-hidden bg-amber-300 p-8 m-8 rounded-2xl shadow-lg flex flex-row items-center justify-between">
+      <div className="flex flex-col max-w-lg z-10">
+        <h1 className="text-4xl font-extrabold mb-2 text-black">
+          Chat smarter, not harder 🚀
+        </h1>
+        <p className="text-lg text-gray-700">
+          Meet <span className="font-semibold">ChatPik</span> — your personal AI
+          companion built for speed, simplicity, and real-time responses. Start
+          now and see how effortless AI conversations can be.
+        </p>
+      </div>
+      <Link href={"/chat"}>
+        <Button
+          variant="default"
+          size="lg"
+          className="px-8 py-4 text-lg font-semibold rounded-xl mt-6 transition-colors duration-200 ease-in-out 
+                   shadow-lg hover:cursor-pointer hover:text-white hover:bg-zinc-800"
+        >
+          Try ChatPik{" "}
+          <svg
+            className="w-7 h-7 ml-2 transition-colors duration-200 ease-in-out fill-zinc-800 hover:fill-amber-50"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="28"
+            height="28"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M19 12H5m14 0-4 4m4-4-4-4"
+            />
+          </svg>
+        </Button>
+      </Link>
+    </div>
+  );
+};
+
+export default CTA;
