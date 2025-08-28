2cd0eccec383a6bfb22c4db60980eaeaedad8955
Syed Hashir Ali <pikaop125@gmail.com>
2025-08-28 21:02:06 +0500

    Added Mongoose for DB connection

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,20 +1,29 @@
 import { NextResponse } from "next/server";
+import mongoose from "mongoose";
+// export async function POST(req) {
+//   let user = await req.json();
+//   if (!user || !user.name || !user.createdAt) {
+//     return NextResponse.json(
+//       { error: "Name and createdAt are required." },
+//       { status: 400 }
+//     );
+//   }
+//   // Process data pending : Save to DB
 
-export async function POST(req) {
-  let user = await req.json();
-  if (!user || !user.name || !user.createdAt) {
-    return NextResponse.json(
-      { error: "Name and createdAt are required." },
-      { status: 400 }
-    );
+//   return NextResponse.json(
+//     {
+//       message: "User Added",
+//       user: `${user.name} and created at: ${user.createdAt}:00`,
+//     },
+//     { status: 200 }
+//   );
+// }
+
+async function main() {
+  try {
+    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+    console.log("MongoDb Connected")
+  } catch (error) {
+    console.log("Error", error);
   }
-  // Process data pending : Save to DB
-
-  return NextResponse.json(
-    {
-      message: "User Added",
-      user: `${user.name} and created at: ${user.createdAt}:00`,
-    },
-    { status: 200 }
-  );
 }
