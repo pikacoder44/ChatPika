e339cb1cd2b2eb431b2125ae87dc5a3e9ffe17e1
Syed Hashir Ali <pikaop125@gmail.com>
2025-08-29 16:04:03 +0500

    Successfully created API route for signup

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,14 +1,31 @@
 import { NextResponse } from "next/server";
 import mongoose from "mongoose";
 import { connectDB } from "@/lib/db";
-
+import User from "@/lib/models/User";
 
 export async function GET() {
   try {
     await connectDB();
 
-    console.log("MongoDb Connected");
+
+    const user = await User.find();
+    return NextResponse.json({ User: user }, { status: 200 });
   } catch (error) {
     console.log("Error", error);
+    return NextResponse.json({ Error: error }, { status: 500 });
+  }
+}
+
+export async function POST(req) {
+  try {
+    await connectDB();
+
+    let payload = await req.json();
+
+    let response = await User.create(payload);
+    return NextResponse.json(response, { status: 201 });
+  } catch (error) {
+    console.log("Error", error);
+    return NextResponse.json({ Error: error }, { status: 500 });
   }
 }
--- lib/models/User.ts
+++ lib/models/User.ts
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
+import { models } from "mongoose";
 const userSchema = new mongoose.Schema({
   name: String,
   createdAt: Number,
 });
 
-const User = mongoose.model("User", userSchema);
+const User = models.User || mongoose.model("User", userSchema);
 export default User;
