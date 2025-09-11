d4e1f923cb7cf2c52d3df56f28a0a51996242108
pikacoder44 <pikaop125@gmail.com>
2025-09-12 00:58:35 +0500

    changed theme to avoid build error

--- components/ChatWindow.tsx
+++ components/ChatWindow.tsx
@@ -1,6 +1,6 @@
 import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { materialOceanic } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 import * as ScrollArea from "@radix-ui/react-scroll-area";
 import { useRef, useEffect } from "react";
 import TypingIndicator from "./TypingIndicator";
@@ -53,7 +53,7 @@ const ChatWindow = ({ messages, streaming }) => {
                             },
                           }}
                           wrapLines={true}
-                          style={materialOceanic}
+                          style={oneDark}
                           language={match[1]}
                           PreTag="div"
                           className="rounded-md my-2"
